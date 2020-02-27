937d30dead83cac3ebdec9e7b6a98d772f4d4c7a
Bilusyak Dmytro <56471886+JanusDG@users.noreply.github.com>
2020-02-27 14:36:45 +0200

    cleaned API keys

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "Bt2pvkfXw6VBHsJwkILook7U3",
-            "consumer_secret": "wih2KnPl38RywPlljSzGx5I0et7yVhsnPUYPfNLqLJRpmsR1M3",
-            "token_key": "1888836979-nIANvsH9ZAdAYxMneeok65lm9kMqhTljWyUATVE",
-            "token_secret": "7KaIP8z04uXj0FwktE87BS5jYtdpZCemjiyBtWTSGLv5J"}
+    return {"consumer_key": "",
+            "consumer_secret": "",
+            "token_key": "",
+            "token_secret": ""}
